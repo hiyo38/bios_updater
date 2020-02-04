d0fc95c82fe45e62f1ca5fbae5d91035d5cdaf9f
root <root@vincent.localdomain>
2020-02-04 13:53:15 +0000

    Changed Main to updater.py

--- updater.py
+++ updater.py
@@ -7,12 +7,18 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download given firmware from model name')
     parser.add_argument('Model Name', nargs='*') 
+    # parser.add_argument('Default',nargs='?',const=1)
     args = parser.parse_args()
     temp=vars(args).values()
     names=[]
     for i in temp:
        for name in i:
             names.append(name)	
+    if  len(names) == 0:
+       f = open("motherboards.txt","r")
+       motherboards= f.readlines()
+       for line in motherboards:
+           names.append(line.strip())
     # Scrapes web page for names and software IDs
     modelList = mobo_drivers.get_motherboard_list()
     # Creates data base
