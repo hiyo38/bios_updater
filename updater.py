08d69c8a08d08a370d585eef992b0d1482d7bfdf
Vincent Moreschi <vincent.m@hostdime.com>
2020-02-24 09:04:38 -0500

    Update updater.py

--- updater.py
+++ updater.py
@@ -6,6 +6,12 @@ import argparse
 import os
 
 if __name__ == "__main__":
+    # Scrapes web page for names and software IDs
+    modelList = mobo_drivers.get_motherboard_list()
+    # Creates data base
+    moboDB.createDB()
+    # Takes list created by get_motherboard_list and generates rows in the DB
+    moboDB.addMOBOS(modelList)
     parser = argparse.ArgumentParser(description='Download given firmware from model name')
     parser.add_argument('Model Name', nargs='*') 
     # parser.add_argument('Default',nargs='?',const=1)
@@ -21,18 +27,13 @@ if __name__ == "__main__":
           mobo_drivers.auto_download()
     except:
        print("Downloading from motherboards.txt")
-    if  names != 'auto':
-        if len(names) == 0:
-           f = open(os.path.dirname(__file__)+ '/motherboards.txt',"r")
-           motherboards= f.readlines()
-           for line in motherboards:
-               names.append(line.strip())
-        # Scrapes web page for names and software IDs
-        modelList = mobo_drivers.get_motherboard_list()
-        # Creates data base
-        moboDB.createDB()
-        # Takes list created by get_motherboard_list and generates rows in the DB
-        moboDB.addMOBOS(modelList)
+       if  names != 'auto':
+           if len(names) == 0:
+              f = open(os.path.dirname(__file__)+ '/motherboards.txt',"r")
+              motherboards= f.readlines()
+              for line in motherboards:
+                  names.append(line.strip())
+        
         # Query DB with model name, returns ID
         softwareID = moboDB.getMOBO(*names)
         # Pass in name and software ID to download the firmware
